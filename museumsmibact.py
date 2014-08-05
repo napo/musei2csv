20fc2aaf614336971462e8cf41ce6d206564b42c
napo <napo@fbk.eu>
2014-08-06 00:24:39 +0200

    piersoft è un grandissimo lamer

--- museumsmibact.py
+++ museumsmibact.py
@@ -1,12 +1,14 @@
 import urllib2
 from lxml import etree
 import csv
+import django.utils.encoding as djenc
+url = "http://dbunico20.beniculturali.it/DBUnicoManagerWeb/dbunicomanager/searchPlace?modulo=luoghi&tipologiaLuogo=1&stato=P&quantita=1&offset=0"
+url = "http://dbunico20.beniculturali.it/DBUnicoManagerWeb/dbunicomanager/searchPlace?modulo=luoghi&stato=P&quantita=1&offset=0"
 
-url = "http://dbunico20.beniculturali.it/DBUnicoManagerWeb/dbunicomanager/searchPlace?modulo=luoghi&tipologiaLuogo=1&stato=P&quantita=100&offset=5"
 xml = urllib2.urlopen(url)
 root = etree.parse(xml)
 totmuseums = int(root.getroot().attrib['totale'])
-limit = 1000
+limit = 100
 steps=totmuseums/limit
 
 
@@ -20,26 +22,42 @@ for s in range(steps):
     idx.append(s*limit)
 step = 0
 print idx
-url = "http://dbunico20.beniculturali.it/DBUnicoManagerWeb/dbunicomanager/searchPlace?modulo=luoghi&tipologiaLuogo=1&stato=P&offset=%s" % (idx[0]) 
-#url = "http://dbunico20.beniculturali.it/DBUnicoManagerWeb/dbunicomanager/searchPlace?modulo=luoghi&tipologiaLuogo=1&stato=P&offset=0&quantita=50" 
+url = "http://dbunico20.beniculturali.it/DBUnicoManagerWeb/dbunicomanager/searchPlace?modulo=luoghi&stato=P&offset=%s" % (idx[0]) 
+#url = "http://dbunico20.beniculturali.it/DBUnicoManagerWeb/dbunicomanager/searchPlace?modulo=luoghi&tipologiaLuogo=1&stato=P&offset=0&quantita=1000" 
 
 xml = urllib2.urlopen(url)
 docxml = etree.parse(xml).findall("mibac")
 with open('musei.csv', 'wb') as csvfile:
     museiwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    museiwriter.writerow(["nome","indirizzo","comune","provincia","sitoweb","latitudine","longitudine"])
+    museiwriter.writerow(["nome","indirizzo","comune","provincia","cap","sitoweb","latitudine","longitudine"])
     for mibac in docxml:
         print step
         writerow = []
-        metainfo = mibac.find('metainfo').getchildren()
-        stato = metainfo[0].getchildren()[0].text
-        enteCompilatore = metainfo[0].getchildren()[1].text
-        nomeRedattore = metainfo[0].getchildren()[2].text
-        nomeCapoRedattore = metainfo[0].getchildren()[3].text
-        dataValidazione = metainfo[0].getchildren()[4].text
-        dataUltimaModifica  = metainfo[0].getchildren()[5].text
-        datacreazionexml = metainfo[1].text
-        sorgente = metainfo[2].text
+        metainfo = mibac.find('metainfo')
+        stato = ''
+        if (metainfo.find('workflow/stato') != None):
+            stato = metainfo.find('workflow/stato').text
+        enteCompilatore = ''
+        if (metainfo.find('workflow/enteCompilatore') != None):
+            enteCompilatore = metainfo.find('workflow/enteCompilatore').text
+        nomeRedattore = ''
+        if (metainfo.find('workflow/nomeRedattore') != None):
+            nomeRedattore = metainfo.find('workflow/nomeRedattore').text
+        nomeCapoRedattore = ''
+        if (metainfo.find('workflow/nomeCapoRedattore') != None):
+            nomeCapoRedattore = metainfo.find('workflow/nomeCapoRedattore').text
+        dataValidazione = ''
+        if (metainfo.find('workflow/dataValidazione') != None):
+            dataValidazione = metainfo.find('workflow/dataValidazione').text
+        dataUltimaModifica = ''
+        if (metainfo.find('workflow/dataUltimaModifica') != None):
+            dataValidazione = metainfo.find('workflow/dataUltimaModifica').text
+        datacreazionexml = ''
+        if (metainfo.find('datacreazionexml') != None):
+            datacreazionexml = metainfo.find('datacreazionexml').text
+        sorgente =''
+        if (metainfo.find('sorgente') != None):
+            datacreazionexml = metainfo.find('sorgente').text
         luogodellacultura = mibac.find("luogodellacultura")
         codice = luogodellacultura.find("identificatore/codice").text
         tipologiaprevalente = luogodellacultura.find("tipologie").attrib['tipologiaPrevalente']
@@ -54,48 +72,58 @@ with open('musei.csv', 'wb') as csvfile:
         sinomimi = luogodellacultura.find("denominazione/sinonimi").getchildren()
         if len(sinomimi) > 0:
             print "altri sinomimi" 
-        denominazione = luogodellacultura[4].getchildren()
-        descrizione = luogodellacultura.find("descrizione/testostandard").text
-        traduzioni = luogodellacultura.find("descrizione/traduzioni").getchildren()
-        if len(traduzioni) > 0:
-            print "altre traduzioni"
+        descrizione = ''
+        if (luogodellacultura.find("descrizione/testostandard") != None):
+            descrizione = luogodellacultura.find("descrizione/testostandard").text
+            traduzioni = luogodellacultura.find("descrizione/traduzioni").getchildren()
+            if len(traduzioni) > 0:
+                print "altre traduzioni"
+
         info = luogodellacultura.find("info")
-        orario = info.find("orario/testostandard").text
-        orario_traduzioni = info.find("orario/traduzioni").getchildren()
-        if (len(orario_traduzioni) > 0):
-            print "orario multilingue"
-        responsabile = info.find("responsabile").text
-        accessibilita = info.find("accessibilita").text
-        sitoweb = info.find("sitoweb").text
-        email = info.find("email").text
-        email_certificata = ""
-        email_certificata = info.find("email-certificata").text
-        telefono = info.find("telefono/testostandard").text
-        telefono_traduzioni = info.find("telefono/traduzioni").getchildren()
-        if (len(telefono_traduzioni)) > 0:
-            print "altre traduzioni telefono"
-        fax = info.find("fax/testostandard").text
-        fax_traduzioni = info.find("fax/traduzioni").getchildren()
-        if (len(fax_traduzioni)) > 0:
-            print "altre traduzioni fax" 
-        chiusurasettimanale = info.find("chiusuraSettimanale/testostandard").text
-        chiusurasettimanale_traduzioni = info.find("chiusuraSettimanale/traduzioni").getchildren()
-        if (len(chiusurasettimanale_traduzioni)) > 0:
-            print "altre traduzioni chiusuraSettimanale"       
-        entecompetente = luogodellacultura.find("enteCompetente/denominazione").text
-        ruolo_entecompetente = luogodellacultura.find("enteCompetente").attrib['ruolo']
-        codici = luogodellacultura.find("enteCompetente/identificatore").getchildren()
-        codice_entecompetente_dbunico20 = ""   
-        codice_entecompetente_mibac = ""
-        for codice in codici:
-            if (codice.attrib['sorgente']=='DBUnico2.0'):
-                codice_entecompetente_dbunico20 = codice.text
-            if (codice.attrib['sorgente']=='MiBAC'):
-                codice_entecompetente_mibac = codice.text
-        entegestore = ""
-        ruolo_entegestore = ""
-        codice_entegestore_dbunico20 = ""
-        codice_entegestore_mibac = ""
+        orario = ""
+        orario_traduzioni = ""
+        if (info != None):
+            if (info.find("orario/testostandard") != None):
+                orario = info.find("orario/testostandard").text
+                orario_traduzioni = info.find("orario/traduzioni").getchildren()
+                if (len(orario_traduzioni) > 0):
+                    print "orario multilingue"
+            responsabile = info.find("responsabile").text
+            accessibilita = info.find("accessibilita").text
+            sitoweb = info.find("sitoweb").text
+            email = info.find("email").text
+            email_certificata = ""
+            email_certificata = info.find("email-certificata").text
+            telefono = info.find("telefono/testostandard").text
+            telefono_traduzioni = info.find("telefono/traduzioni").getchildren()
+            if (len(telefono_traduzioni)) > 0:
+                print "altre traduzioni telefono"
+            fax = info.find("fax/testostandard").text
+            fax_traduzioni = info.find("fax/traduzioni").getchildren()
+            if (len(fax_traduzioni)) > 0:
+                print "altre traduzioni fax" 
+            if (info.find("chiusuraSettimanale/testostandard") != None):
+                chiusurasettimanale = info.find("chiusuraSettimanale/testostandard").text
+                chiusurasettimanale_traduzioni = info.find("chiusuraSettimanale/traduzioni").getchildren()
+                if (len(chiusurasettimanale_traduzioni)) > 0:
+                    print "altre traduzioni chiusuraSettimanale" 
+
+        if (luogodellacultura.find("enteCompetente") != None):
+            entecompetente = luogodellacultura.find("enteCompetente/denominazione").text
+            ruolo_entecompetente = luogodellacultura.find("enteCompetente").attrib['ruolo']
+            codici = luogodellacultura.find("enteCompetente/identificatore").getchildren()
+            codice_entecompetente_dbunico20 = ""   
+            codice_entecompetente_mibac = ""
+            for codice in codici:
+                if (codice.attrib['sorgente']=='DBUnico2.0'):
+                    codice_entecompetente_dbunico20 = codice.text
+                if (codice.attrib['sorgente']=='MiBAC'):
+                    codice_entecompetente_mibac = codice.text
+            entegestore = ""
+            ruolo_entegestore = ""
+            codice_entegestore_dbunico20 = ""
+            codice_entegestore_mibac = ""
+            
         if (luogodellacultura.find("enteGestore/denominazione") != None):
             entegestore = luogodellacultura.find("enteGestore/denominazione").text
             ruolo_entegestore = luogodellacultura.find("enteGestore").attrib['ruolo']
@@ -107,6 +135,7 @@ with open('musei.csv', 'wb') as csvfile:
                     codice_entegestore_dbunico20 = codice.text
                 if (codice.attrib['sorgente']=='MiBAC'):
                     codice_entegestore_mibac = codice.text
+
         contenitori = luogodellacultura.find("contenitori").getchildren()
         if (len(contenitori)>0):
             print "Ci sono contenitori"
@@ -169,6 +198,9 @@ with open('musei.csv', 'wb') as csvfile:
         if (indirizzo.find('comune') != None):
             comune_indirizzo = indirizzo.find('comune').text
             codistat_comune_indirizzo = indirizzo.find('comune').attrib['istat']
+        cap_indirizzo = ""
+        if (indirizzo.find('cap') != None):
+            comune_indirizzo = indirizzo.find('cap').text
         provincia_indirizzo = ""
         codistat_provincia_indirizzo = ""
         if (indirizzo.find('provincia') != None):
@@ -179,7 +211,6 @@ with open('musei.csv', 'wb') as csvfile:
             codistat_regione_indirizzo = indirizzo.find('regione').attrib['istat'] 
         latitudine = ""
         longitudine = ""
-        print indirizzo.getchildren()
         if (indirizzo.find('cartografia') != None):
             latitudine = indirizzo.find('cartografia/punto/latitudineX').text
             longitudine = indirizzo.find('cartografia/punto/longitudineY').text
@@ -189,12 +220,13 @@ with open('musei.csv', 'wb') as csvfile:
         #print allegati.getchildren()
         step += 1
         if (longitudine != ""):
-            writerow.append(nome)        
-            writerow.append(via_indirizzo)
-            writerow.append(comune_indirizzo)
-            writerow.append(provincia_indirizzo)
-            writerow.append(sitoweb)
-            writerow.append(latitudine)
-            writerow.append(longitudine)
+            writerow.append(djenc.smart_str(nome))        
+            writerow.append(djenc.smart_str(via_indirizzo))
+            writerow.append(djenc.smart_str(comune_indirizzo))
+            writerow.append(djenc.smart_str(provincia_indirizzo))
+            writerow.append(djenc.smart_str(cap_indirizzo))
+            writerow.append(djenc.smart_str(sitoweb))
+            writerow.append(djenc.smart_str(latitudine))
+            writerow.append(djenc.smart_str(longitudine))
             museiwriter.writerow(writerow)
 
